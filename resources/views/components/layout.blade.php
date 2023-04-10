2eea9afeb45575f8af3d79f872d7d7b3537d82ce
Krit <tanakritkhunkhae@gmail.com>
2023-04-11 00:00:56 +0700

    bladew ind

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -1,12 +1,19 @@
 <!DOCTYPE html>
 
+
+<link href="{{ asset('vendor/bladewind/css/animate.min.css') }}" rel="stylesheet" />
+
+<link href="{{ asset('vendor/bladewind/css/bladewind-ui.min.css') }}" rel="stylesheet" />
+
+<script src="{{ asset('vendor/bladewind/js/helpers.js') }}"></script>
 <link rel="stylesheet" href="/app.css">
 
 <title> Learn-Laravel </title>
-
+<head><script defer src="https://unpkg.com/alpinejs@3.x.x/dist/cdn.min.js"></script></head>
 <body>
 
-    {{ $slot }}
-
+    {{-- {{ $slot }} --}}
+    
+    <x-bladewind::button>Save User</x-bladewind::button>
     {{-- @yield('content') --}}
 </body>
